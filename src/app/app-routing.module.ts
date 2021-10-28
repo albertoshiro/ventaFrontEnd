5a2f7519416b67186e14c0c9177fe29c71e97a52
Alberto Monterrubio <alberto.toshiro@gmail.com>
2021-10-28 17:09:19 -0500

    enrutamiento y creacion de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
+import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [];
+//aqui es donde enrutaremos, como un objeto en una lista o json
+const routes: Routes = [
+  //en caso de vacios en las url
+  {path:"",redirectTo:'/home', pathMatch:'full'},
+  {path:"home" , component : HomeComponent},
+  {path:"customer",component: CustomerComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { CustomerComponent } from './customer/customer.component';
+//este es para menus de material
+import { MatSidenavModule } from '@angular/material/sidenav'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CustomerComponent
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
